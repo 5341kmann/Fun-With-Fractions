b7c7a8487cf6c258c0ec41f31f731f76d775b5a0
[Grant Sackmann]] <[grant.sackmann@icloud.com]>
2024-09-21 15:04:43 -0500

    update

--- fun_w_fractions/src/main/java/edu/grinnell/csc207/util/BFCalculator.java
+++ fun_w_fractions/src/main/java/edu/grinnell/csc207/util/BFCalculator.java
@@ -1,5 +1,24 @@
 package edu.grinnell.csc207.util;
 
 public class BFCalculator {
+  public BigFraction get() {
+    return new BigFraction();
+  }
+
+  public void add(BigFraction val){
+    
+  }
+public void subtract(BigFraction val) {
+
+}
+public void multiply(BigFraction val){
+
+}
+public void divide(BigFraction val) {
+
+}
+
+public void clear() {
   
+}
 }
